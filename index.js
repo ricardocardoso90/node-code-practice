37ed1a2d255754eeaa02d815e990398d9ec312b1
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-03-18 16:00:49 -0300

    update commit

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 import chalk from "chalk";
 import express from "express";
 
-console.log(chalk.green("Starting app in dev mode..."));
-
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-//req = requisição: são dados que recebemos.
-//res = resposta: são dados que enviamos.
+const checkUsers = (req, res, next) => {
+  req.authStatus = true;
+
+  req.authStatus
+    ? console.log(chalk.bgGreen.black("Você está logado!!"))
+    : console.log(chalk.bgRed.black("Você não está logado, faça login para continuar!!"))
+
+  next();
+};
+
+app.use(checkUsers);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World");
 });
 
-const name = "Ricardo";
+app.get("/users/:params", (req, res) => {
+  res.send("Página de usuários!!");
 
-app.get("/users", (req, res) => {
-  res.send(`Olá, ${name}`);
+  console.log(chalk.green("Buscando usuário..."));
+  console.log(req.params);
 });
 
-app.listen(port, () => {
-  console.log(`App rodando na porta ${port}`);
+app.listen(PORT, () => {
+  console.log(chalk.green(`Servidor rodando na porte ${PORT}.`));
 });
