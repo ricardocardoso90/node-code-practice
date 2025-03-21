1027a95d035a177e02686c4bf67bd79b97edc7bc
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-03-21 13:34:14 -0300

    update commit

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-import chalk from "chalk";
-import express from "express";
+const path = require("path");
+const express = require("express");
+const router = require("./routes");
 
 const app = express();
 const PORT = 3000;
 
-const checkUsers = (req, res, next) => {
-  req.authStatus = true;
+const basePath = path.join(__dirname, './templates');
 
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(router);
+
+router.get("/", (req, res) => {
+  res.sendFile(`${basePath}/index.html`)
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.sendFile(`${basePath}/error.html`);
+});
+
+const checkUsers = (req, res, next) => {
+  res.sendFile(`${basePath}/userform.html`);
+
+  req.authStatus = true;
   req.authStatus
-    ? console.log(chalk.bgGreen.black("Você está logado!!"))
-    : console.log(chalk.bgRed.black("Você não está logado, faça login para continuar!!"))
+    ? console.log("Você está logado!!")
+    : console.log("Você não está logado!!")
 
   next();
 };
-
 app.use(checkUsers);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/users/:params", (req, res) => {
-  res.send("Página de usuários!!");
-
-  console.log(chalk.green("Buscando usuário..."));
-  console.log(req.params);
-});
-
 app.listen(PORT, () => {
-  console.log(chalk.green(`Servidor rodando na porte ${PORT}.`));
+  console.log(`Servidor rodando na porte ${PORT}.`);
 });
