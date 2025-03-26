3fb312939c6f40deff319cb2ed3e8f34d8e92d44
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-03-26 16:01:20 -0300

    update commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const exphbs = require('express-handlebars');
 
 const router = require("./routes");
 
+// const hbs = exphbs.create({
+//   partialsDir: ["views/partials"],
+// });
+
 const app = express();
 const PORT = 3000;
 
@@ -23,9 +27,44 @@ app.use(router);
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+app.get("/dashboard", (req, res) => {
+  // const games = ["Dark Souls", "eFootball", "Elden Ring", "Demon`s Souls", "Rocket League"];
+
+  const games = [
+    { id: 1, nome: "Dark Souls" },
+    { id: 2, nome: "eFootball" },
+    { id: 3, nome: "Elden Ring" },
+    { id: 4, nome: "Demon`s Souls" },
+    { id: 5, nome: "Rocket League" }
+  ];
+
+  res.render('dashboard', { games });
+});
+
+app.get("/blogpost", (req, res) => {
+  const post = {
+    title: "Aula de NODEJS",
+    category: "JavaScript",
+    body: "Aulas para aprender BackEnd com NodeJS!",
+  }
+
+  res.render('blogpost', { post });
+});
+
+app.get("/blog", (req, res) => {
+  res.render("blog");
+});
+
 app.get("/", (req, res) => {
+  const isLoaded = true;
+
+  const user = {
+    name: "Ricardo",
+    surname: "Cardoso"
+  };
+
+  res.render('home', { user, isLoaded });
   // res.sendFile(`${basePath}/index.html`)
-  res.render('home');
   // res.render('./views/layouts/main.handlebars', { layout: false });
 });
 
