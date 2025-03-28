c8c6076ea087b02442529f84222c16132a7087b9
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-03-27 21:47:51 -0300

    update commit

--- 2_Aula/index.js
+++ 2_Aula/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const app = express();
 
 const mysql = require('mysql');
 const exphbs = require('express-handlebars');
 
-const app = express();
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
 
 app.set("view engine", "handlebars");
 app.engine("handlebars", exphbs.engine());
@@ -12,6 +19,29 @@ app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.post("/books/insertbook", (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const querySql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
+  conn.query(querySql, (error) => {
+    error
+      ? console.log(error)
+      : res.redirect("/books");
+  });
+});
+
+app.get("/books", (req, res) => {
+  const querySqlReq = "SELECT * FROM books";
+  conn.query(querySqlReq, (error, data) => {
+
+    error && console.log(error);
+    console.log(data);
+
+    res.render("books", { data });
+  });
 });
 
 const conn = mysql.createConnection({
