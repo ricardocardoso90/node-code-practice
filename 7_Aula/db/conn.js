4757267e814944c0315c703a23e4d09c64ced34f
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-04-13 02:06:03 -0300

    update commit

--- 7_Aula/db/conn.js
+++ 7_Aula/db/conn.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('nodesequelize', 'root', '', {
+const sequelize = new Sequelize('nodemvc', 'root', '', {
   host: 'localhost',
   dialect: "mysql",
 });
 
-// try {
-//   sequelize.authenticate();
-//   console.log("Conectado ao sequelize!!");
-// } catch (error) {
-//   console.log("Não foi possível conectar:", error);
-// };
+try {
+  sequelize.authenticate();
+  console.log("Conectado ao MySQL!!");
+} catch (error) {
+  console.log(`Não foi possível conectar: ${error}`);
+};
 
 module.exports = sequelize;
---
+++ 7_Aula/routes/tasksRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const TaskController = require('../controllers/TaskController');
+
+router.get('/', TaskController.showTask);
+router.get('/add', TaskController.createTask);
+
+module.exports = router;
