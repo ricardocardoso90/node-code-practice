088c471299c41bf5243e8e96493463e88323a581
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-05-16 18:57:19 -0300

    update commit

--- 8_Aula/routes/productsRoutes.js
+++ 8_Aula/routes/productsRoutes.js
@@ -5,4 +5,7 @@ const ProductController = require('../controllers/ProductController');
 
 router.get('/', ProductController.showProducts);
 
+router.get('/create', ProductController.createProduct);
+router.post('/create', ProductController.createProductPost);
+
 module.exports = router;
