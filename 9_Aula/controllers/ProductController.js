d66d15c21b270e035cf69eb88fb6f6b65b29be05
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-05-19 14:42:32 -0300

    update commit

--- 9_Aula/controllers/ProductController.js
+++ 9_Aula/controllers/ProductController.js
@@ -23,12 +23,12 @@ module.exports = class ProductController {
     res.redirect("/products");
   };
 
-  // static async getProduct(req, res) {
-  //   const id = req.params.id;
-  //   const product = await Product.getProductById(id);
+  static async getProduct(req, res) {
+    const id = req.params.id;
+    const product = await Product.findById(id).lean();
 
-  //   res.render('products/product', { product });
-  // };
+    res.render('products/product', { product });
+  };
 
   // static async deleteProduct(req, res) {
   //   const id = req.params.id;
