f5ef70de6ae2a3fe989d00772cc36dc67ffa7865
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-05-19 00:54:33 -0300

    update commit

--- 9_Aula/controllers/ProductController.js
+++ 9_Aula/controllers/ProductController.js
@@ -1,59 +1,59 @@
 const Product = require('../models/Product');
 
 module.exports = class ProductController {
-  static async showProducts(req, res) {
-    const products = await Product.getProducts();
+  // static async showProducts(req, res) {
+  //   const products = await Product.getProducts();
 
-    res.render("products/all", { products });
-  };
+  //   res.render("products/all", { products });
+  // };
 
   static createProduct(req, res) {
     res.render("products/create");
   };
 
-  static createProductPost(req, res) {
+  static async createProductPost(req, res) {
     const name = req.body.name;
     const image = req.body.image;
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(name, image, price, description);
-    product.save();
+    const product = new Product({ name, image, price, description });
+    await product.save();
 
     res.redirect("/products");
   };
 
-  static async getProduct(req, res) {
-    const id = req.params.id;
-    const product = await Product.getProductById(id);
+  // static async getProduct(req, res) {
+  //   const id = req.params.id;
+  //   const product = await Product.getProductById(id);
 
-    res.render('products/product', { product });
-  };
+  //   res.render('products/product', { product });
+  // };
 
-  static async deleteProduct(req, res) {
-    const id = req.params.id;
-    await Product.deleteProduct(id);
+  // static async deleteProduct(req, res) {
+  //   const id = req.params.id;
+  //   await Product.deleteProduct(id);
 
-    res.redirect('/products');
-  };
+  //   res.redirect('/products');
+  // };
 
-  static async editProduct(req, res) {
-    const id = req.params.id;
-    const product = await Product.getProductById(id);
+  // static async editProduct(req, res) {
+  //   const id = req.params.id;
+  //   const product = await Product.getProductById(id);
 
-    res.render('products/edit', { product });
-  };
+  //   res.render('products/edit', { product });
+  // };
 
-  static async updateProduct(req, res) {
-    const id = req.body.id;
-    const name = req.body.name;
-    const image = req.body.image;
-    const price = req.body.price;
-    const description = req.body.description;
+  // static async updateProduct(req, res) {
+  //   const id = req.body.id;
+  //   const name = req.body.name;
+  //   const image = req.body.image;
+  //   const price = req.body.price;
+  //   const description = req.body.description;
 
-    const product = new Product(name, image, price, description);
-    await product.updateProduct(id);
+  //   const product = new Product(name, image, price, description);
+  //   await product.updateProduct(id);
 
-    res.redirect('/products');
-  };
+  //   res.redirect('/products');
+  // };
 };
--- 9_Aula/routes/productsRoutes.js
+++ 9_Aula/routes/productsRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const ProductController = require('../controllers/ProductController');
 
-router.get('/', ProductController.showProducts);
+// router.get('/', ProductController.showProducts);
 
 router.get('/create', ProductController.createProduct);
 router.post('/create', ProductController.createProductPost);
 
-router.get('/:id', ProductController.getProduct);
-router.post('/delete/:id', ProductController.deleteProduct);
+// router.get('/:id', ProductController.getProduct);
+// router.post('/delete/:id', ProductController.deleteProduct);
 
-router.get('/edit/:id', ProductController.editProduct);
-router.post('/edit', ProductController.updateProduct);
+// router.get('/edit/:id', ProductController.editProduct);
+// router.post('/edit', ProductController.updateProduct);
 
 module.exports = router;
