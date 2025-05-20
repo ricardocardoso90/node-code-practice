dabcc2abc35accc78e1121a9c0f350116a618e07
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-05-20 16:27:25 -0300

    update commit

--- 10_Aula/index.js
+++ 10_Aula/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.render("Olá Mundão!!");
+  res.send("Olá Mundão!!");
+  // res.json({ message: "Primeira rota criada com sucesso!!" })
 });
 
 app.listen(3000, () => console.log("Servidor rodando com sucesso!!"));
