26655267511fe87a1352f98e99fc1c6d46257b9f
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-05-20 21:43:30 -0300

    update commit

--- 10_Aula/index.js
+++ 10_Aula/index.js
@@ -10,15 +10,20 @@ app.use(
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send("Teste de API!!");
+  res.status(200).send("Teste de API!!");
 });
 
 app.post('/createproduct', (req, res) => {
   const name = req.body.name;
   const price = req.body.price;
 
+  if (!name) {
+    res.status(422).json({ message: "Algo deu errado!!" });
+    return
+  };
+
   console.log(`Nome: ${name}, preço: ${price}`);
-  res.json({ message: "Dados enviados com sucesso!!" });
+  res.status(201).json({ message: "Dados enviados com sucesso!!" });
 });
 
 app.listen(3000, () => console.log("Servidor rodando com sucesso!!"));
